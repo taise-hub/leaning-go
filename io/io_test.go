913df31404eb0339064206ae5ba305ece7c9f982
井上大誠 <t.base.8901@icloud.com>
2021-07-07 09:50:48 +0900

    test io/CopyBuffer

--- io/io_test.go
+++ io/io_test.go
@@ -14,7 +14,6 @@ func Test_Copy(t *testing.T) {
 	}
 	tests := map[string]struct {
 		args args
-		expected int64
 	}{
 		"標準出力": {
 			args: args{
@@ -26,6 +25,32 @@ func Test_Copy(t *testing.T) {
 	for tName, test := range tests {
 		t.Run(tName, func(t *testing.T) {
 			if _, err := io.Copy(test.args.dst, test.args.src); err != nil {
+				panic(err)
+			}
+		})
+	}
+}
+
+func Test_CopyBuffer(t *testing.T) {//Copyとほぼ変わらない。bufferを自分で用意する点が違う？
+	type args struct {
+		dst io.Writer
+		src io.Reader
+		buf []byte
+	}
+	tests := map[string]struct {
+		args args
+	}{
+		"一般的な使い方": {
+			args{
+				src: strings.NewReader("first reader\n"),
+				dst: os.Stdout,
+				buf: make([]byte, 1),
+			},
+		},
+	}
+	for tName, test := range tests {
+		t.Run(tName, func(t *testing.T) {
+			if _, err := io.CopyBuffer(test.args.dst, test.args.src, test.args.buf); err != nil{
 				panic(err)
 			}
 		})
